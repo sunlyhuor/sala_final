d73517eef23b2302df74a5ed9d518a12ac0f235f
chheang-pheaktra <chheangpheaktra154@gmail.com>
2023-12-26 21:39:31 +0700

    modified page datatable

--- assets/js/blogTable.js
+++ assets/js/blogTable.js
@@ -1,73 +1,74 @@
-const url2 = "https://cms.istad.co/api/sala-lessons?populate=thumbnail";
-const tab2 = document.getElementById("tabs2");
-let display = " ";
-//GET tab2
-const renderTable2 = (dis) => {
-  dis.map((table) => {
-    display += `
+const url3 = "https://cms.istad.co/api/sala-blogs?populate=%2A";
+const tabs3 = document.getElementById("tabs3");
+let blog = " ";
+//GET tab3
+const renderTable3 = (dis) => {
+  dis.map((display) => {
+    blog += `
         <tr>
             <td class="px-6 py-4 whitespace-nowrap">
                 <div class="flex items-center">
                     <div class="flex-shrink-0 w-64 h-64">
                          <img class="" src="https://cms.istad.co${
-                           table.attributes.thumbnail.data.attributes.formats
-                             .thumbnail.url
+                           display.attributes.thumbnail?.data?.attributes?.url
                          }" alt="">
                     </div>
                 </div>
             </td>
             <td class="px-6 py-4 whitespace-wrap">
                 <div class="text-sm text-gray-900">${
-                  table.attributes.title
+                  display.attributes?.title
                 }</div>
             </td>
             <td class="px-6 py-4 whitespace-wrap">
                 <div class="text-sm text-gray-900">${
-                  table.attributes.about
+                  display.attributes?.content
                 }</div>
              </td>
             <td class="px-6 py-4 whitespace-nowrap">
                  <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                        ${new Date(table.attributes.createdAt).toDateString()}
+                        ${new Date(display.attributes.createdAt).toDateString()}
                     </span>
             </td>
             <td class="px-6 py-4 whitespace-nowrap  text-sm font-medium">
                  <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                <a href="#" class="ml-2 text-red-600 hover:text-red-900" onclick="deletebook(${
-                  table.id
+                <a href="#" class="ml-2 text-red-600 hover:text-red-900" onclick="deleteBlog(${
+                  display.id
                 })">Delete</a>
             </td>
         </tr>
         `;
   });
 
-  tab2.innerHTML = display;
+  tabs3.innerHTML = blog;
 };
-const deletebook = (bookId) => {
+// Function to delete a blog
+const deleteBlog = (blogId) => {
   // Display a confirmation dialog
-  const isConfirmed = confirm("Are you sure you want to delete this book?");
+  const isConfirmed = confirm("Are you sure you want to delete this blog?");
 
   if (isConfirmed) {
     // Make a DELETE request to the API with the specific blog ID
-    fetch(`https://cms.istad.co/api/sala-lessons/${bookId}`, {
+    fetch(`https://cms.istad.co/api/sala-blogs/${blogId}`, {
       method: "DELETE",
     })
       .then((res) => res.json())
-      .then((result) => {
+      .then((resp) => {
         // Handle the response or update the UI as needed
-        console.log("Book deleted successfully:", result);
+        console.log("Blog deleted successfully:", resp);
         // You may want to re-fetch the data and render the updated table
-        renderTable2(resp); // Add this function to re-fetch and render the updated data
+        renderTable3(resp); // Add this function to re-fetch and render the updated data
       })
       .catch((error) => {
-        console.error("Error deleting book:", error);
+        console.error("Error deleting blog:", error);
       });
   }
-};
+};  
 
-fetch(url2)
+fetch(url3)
   .then((res) => res.json())
   .then((resp) => {
     let results = resp.data;
-    renderTable2(results);
+    renderTable3(results);
+    console.log(results);
   });
